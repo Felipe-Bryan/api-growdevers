9d6f9840b94a87868fbd2f292c0e3b27c996f14c
Felipe Bryan <felipe.bryan@live.com>
2023-05-17 20:27:20 -0300

    primeira rota

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express, { Request, Response } from 'express';
+import { Growdever } from './models/growdever';
+
+const app = express();
+app.get('/', (req: Request, res: Response) => {
+  console.log('entrou na rota');
+  const growdever1 = new Growdever('GD1', 'Felipe', 32);
+  res.status(200).send({ ok: true, data: growdever1.toJson() });
+});
+
+app.listen(3333, () => {
+  console.log('Server running');
+});
